38f7d8a73caaef159911a0aa5bf387dc9d2629b9
albanec <eugeni.maksimov@yandex.ru>
2016-02-14 22:11:28 +0300

    another commit

---
+++ temp/work.projects/MM/F.optimal.R
@@ -0,0 +1,56 @@
+
+
+X <- rbinom(X.length, 1, X.win.rate)
+
+F.calc <- function (X, risk, win.profit, balance ) {
+	# генерация ряда сделок
+			n <- length(X) 
+		# рабочий фрейм
+			b <- rep(NA, n)
+			b[1] <- balance
+			df = data.frame(pos = X, balance = b)	
+		# функция расчёта баланса
+			for (i in 2:n) {
+				df$balance[1] <- balance
+				df$balance[i] <- ifelse( df$pos[i-1] > 0, df$balance[i-1] * (1 + win.profit*risk),
+									df$balance[i-1] * (1 - risk)
+								)	
+			}		
+	profit <- df$balance[[n]]
+	return ( profit )
+}
+
+F.calc2 <- function(X, win.profit, balance, r.hi) {
+	f <- seq(from=0.01, to=r.hi, by=0.01)
+	df.risk = data.frame(f)
+	n <- nrow(df.risk)
+	for (i in 1:n) {
+		df.risk$profit[i] <- F.calc (X, risk = df.risk$f[i], win.profit, balance )
+	}
+	return (df.risk)	
+}
+
+F.calc3 <- function(X, win.profit, balance, r.hi) {
+			f <- seq(from=0.01, to=r.hi, by=0.01)
+			df.risk = data.frame(f)
+			n <- nrow(df.risk)
+			for (i in 1:n) {
+				df.risk$profit[i] <- F.calc( X, risk = df.risk$f[i], win.profit, balance )
+			}
+			y <- which.max(df.risk$profit)
+			return ( c(df.risk$f[y], df.risk$profit[y]) )	
+	}
+
+full.F.test <- function (rep, X.length, X.win.rate, win.profit, balance, r.hi) {
+	z <- rep(NA, rep)
+	t <- data.frame(z)
+	for (i in 1:rep) {
+		X <- rbinom(X.length, 1, X.win.rate)
+		y <- F.calc3(X, win.profit, balance, r.hi)		
+		t$risk[i] <- y[1]
+		t$profit[i] <- y[2]
+	}
+	t$z <- NULL
+	return (t)
+}
+
