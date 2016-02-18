1effa0b446a8928f116f54ed9cc36ed1fa1203b0
albanec <eugeni.maksimov@yandex.ru>
2016-02-19 02:38:39 +0300

    +1

--- temp/work.projects/CSV parse/csv-parse.R
+++ temp/work.projects/CSV parse/csv-parse.R
@@ -1,6 +1,6 @@
 library("quantmod")
-
-# Парсинг .CSV файлов для оптимизации
+library("plotly")
+#
 cat(sep = "\n", "############",  
 				"Парсинг .CSV", 
 				"############", "")
@@ -13,12 +13,9 @@ cat(sep = "\n", "############",
 		var1 <- NA; var2 <- NA; var3 <- NA; profit <- NA
 		cat("Текущий рабочий католог", sep=" .......... ", wd)
 	# путь к файлу
-		cat("Путь к файлy 1: ", "\n")
-		file.path1 <- readline()
-		cat("Путь к файлy 1: ", ".......... ", file.path1, "\n")
-		cat("Путь к файлy 2: ", "\n")
-		file.path2 <- readline()
-		cat("Путь к файлy 2: ", ".......... ", file.path2, "\n")
+		cat("Путь к файлy: ", "\n")
+		file.path <- readline()
+		cat("Путь к файлy: ", ".......... ", file.path, "\n")
 	# номера переменных
 		cat(sep = "\n", "############",
 						"Номера столбцов переменных & теплового параметра:",
@@ -36,20 +33,15 @@ cat(sep = "\n", "############",
 		profit <- readline()
 		cat("profit: ", ".......... ", profit, "\n")
 
-
-parse.csv <- function (file.path1, file.path2, var1=26, var2=27, var3=28, profit) {
+parse.csv <- function (file.path=file.path, var1=26, var2=27, var3=28, profit=profit) {
 	# подгрузка файлов 
-		file1 <- read.table(file=file.path1, header=F, sep = ";", as.is=T) 
-		file2 <- read.table(file=file.path2, header=F, sep = ";", as.is=T) 
-		temp.t <- c( nrow(file1), nrow(file2) )
-		temp.frame <- rep(NA, max(temp.t))
-		temp.frame <- data.frame(temp.frame) 
+		file <- read.table(file=file.path, header=F, sep = ";", as.is=T)   
 
-	# выделядем нужные параметры
-		var1.name <- file1[[1, var1]]
-		var2.name <- file1[[1, var2]]
-		var3.name <- file1[[1, var3]]
-		profit.name <- file1[[1, profit]] 
+	# выделяем нужные параметры
+		var1.name <- file[[1, var1]]
+		var2.name <- file[[1, var2]]
+		var3.name <- file[[1, var3]]
+		profit.name <- file[[1, profit]] 
 		cat(sep = "\n", "############",
 						"Выбраны переменные & тепловой параметр:",
 						"" )
@@ -62,20 +54,53 @@ parse.csv <- function (file.path1, file.path2, var1=26, var2=27, var3=28, profit
 						"Парсинг", 
 						"" )
 	# выборка данных 
-		temp.file1 <- file1
-		temp.file2 <- file2
-		temp.file1[1, ] <- NA
-		temp.file2[1, ] <- NA
-		temp.frame$file1.var1 <- temp.file1[var1] 
-		temp.frame$file1.var2 <- temp.file1[var2] 
-		temp.frame$file1.var3 <- temp.file1[var3]
-		temp.frame$file1.profit <- temp.file1[profit]
-		temp.frame$file2.var1 <- temp.file2[var1]
-		temp.frame$file2.var2 <- temp.file2[var2]
-		temp.frame$file2.var3 <- temp.file2[var3]
-		temp.frame$file2.profit <- temp.file2[profit]
+		# чистим от лишнего
+		file <- file[-1,]
+		file <- file[, colSums(is.na(file)) == 0]
+		#
+		temp.t <- nrow(file) 
+		temp.frame <- rep(NA, max(temp.t))
+		temp.frame <- data.frame(temp.frame)
+		#
+		temp.frame$var1 <- as.numeric( gsub("\\,", ".", file[[var1]]) ) 
+		temp.frame$var2 <- as.numeric( gsub("\\,", ".", file[[var2]]) )
+		temp.frame$var3 <- as.numeric( gsub("\\,", ".", file[[var3]]) )
+		temp.frame$profit <- as.numeric( gsub("\\,", ".", file[[profit]]) )
+		temp.frame$temp.frame <- NULL
+
+	cat(sep = "\n", "############",
+						"Готово.", 
+					"############" )
+	
 	return (temp.frame)
 }	
 
-	
+# загрузка данных
+file <- parse.csv(file.path=file.path, var1=var1, var2=var2, var3=var3, profit=profit)
+# сортировка по профиту
+file <- file[order(-file$profit),]
+
+cat(sep = "\n", "############",
+				"Оптимальная торговля:", 
+					"" )
+cat("var1: ", ".......... ", file[1,1] , "\n")
+cat("var2: ", ".......... ", file[1,2], "\n")
+cat("var3: ", ".......... ", file[1,3], "\n")
+cat("profit: ", ".......... ", file[1,4], "\n")
+
+cat(sep = "\n", "############",
+				"Строим график:", 
+					"" )
+
+# график
+mycolors <-  rainbow(30, start=0.15, end=0.95)
+plot_ly(x=file2$var1, y=file2$var2, z=file2$var3, type="scatter3d", mode="markers", color=file2$profit, colors=mycolors) %>%
+	layout(title = "Тепловая карта",
+         scene = list(
+           xaxis = list(title = var1.name), 
+           yaxis = list(title = var2.name), 
+           zaxis = list(title = var3.name)
+           )
+         )
+
 
