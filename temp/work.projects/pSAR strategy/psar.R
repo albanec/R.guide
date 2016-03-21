ac79601c882cb7b68b23f0c1304f0b5069d6a267
albanec <eugeni.maksimov@yandex.ru>
2016-03-21 17:11:39 +0300

    +1

--- temp/work.projects/pSAR strategy/psar.R
+++ temp/work.projects/pSAR strategy/psar.R
@@ -6,16 +6,19 @@ library("PerformanceAnalytics")
 		###########
 #описание функций 
 #
-# загрузка данных
-	# дата в формате "2015-01-01"
 get.data <- function (ticker, from.date, to.date=Sys.Date(), frame="15min") {
+	# загрузка данных
+	# дата в формате "2015-01-01"
+	print("Download Source Data")
 	data <- getSymbols(ticker, from=from.date, to=to.date, period=frame, src="Finam", auto.assign=FALSE)
 	names(data) <- c("Open" , "High" , "Low" , "Close" , "Volume")
+	
 	return(data)
 }
 #
-# функция для перехода к состояниям (фильтрация)
+
 exrem <- function(x) {
+	# функция для перехода к состояниям (фильтрация сигналов)
 	x$a <- na.locf( x )
 	x$a <- ifelse( is.na(x$a) | is.nan(x$a) | is.infinite(x$a), 0, x$a )
 	ind <- which( x$a != lag(x$a) )
@@ -24,9 +27,10 @@ exrem <- function(x) {
 	x$y[1] <- x$a[1]
 	return(x$y)
 }
-# psar.2sma стратегия 
+
 nameOfStrategy <- "PSAR and 2SMA cross"
 strategy.psar.2sma <- function (data, slow.sma, fast.sma, accel.start=0.02, accel.max=0.2, state=TRUE) {
+	# описание psar.2sma стратегии 
 	data$sma <- SMA(Cl(data), slow.sma)
 	data$fma <- SMA(Cl(data), fast.sma)
 	data$sar <- SAR(data, accel = c(accel.start, accel.max))
@@ -42,13 +46,15 @@ strategy.psar.2sma <- function (data, slow.sma, fast.sma, accel.start=0.02, acce
 	data$pos <- lag(data$pos)
 	if (state==TRUE) {
 		data$state <- exrem(data$pos)
+	} else {
+		colnames(data)[colnames(data)=="pos"] <- "state"
 	}
 	data <- na.omit(data)
 	return(data)
 }		
 
-# расчет доходностей
 calc.returns <- function(data, pip, s0=0, abs=FALSE, SR=FALSE, LR=FALSE, reinvest=TRUE) {
+	# расчет доходностей
 	if (abs==TRUE) { 	
 		if (reinvest==TRUE) {
 			data$w <- data$state[[1]] * s0/data$Open[[1]]
@@ -94,8 +100,9 @@ calc.returns <- function(data, pip, s0=0, abs=FALSE, SR=FALSE, LR=FALSE, reinves
 	}
 	return(data)
 }
-# расчет итогово профита
-profit.calc <- function(data, s0=0, reinvest=TRUE) {
+
+calc.profit <- function(data, s0=0, reinvest=TRUE) {
+	# расчет итогового профита
 	if (reinvest==TRUE) {
 		profit <- as.numeric(last(data$equity) - s0)		
 	} else {
@@ -104,22 +111,36 @@ profit.calc <- function(data, s0=0, reinvest=TRUE) {
 	return (profit)
 }
 # 
-dataset.psar.2sma <- function(data, state, pip, s0, abs, SR, LR, reinvest, ) {
+dataset.psar.2sma <- function(data, state=FALSE, pip=1, s0=1, abs=FALSE, SR=FALSE, LR=FALSE, reinvest=FALSE, ) {
+	# функция формирования итогового фрейма данных по всем наборам переменных работы стратегии
 	firstRun <- TRUE
 	for (fast.sma in seq(1, 99, 1)) {
 		for(slow.sma in seq(fast.sma+1, 100, 1)) {
 			for (accel.max in seq(0.05, 0.4, 0.01)) {
 				for (accel.start in seq(0.01, accel.max, 0.01)) {
 					temp.data <- strategy.psar.2sma(data, state)
-					temp.data <- calc.returns(temp.data, pip, s0, abs, SR, LR, reinvest)
-					profit <- profit.calc(temp.data, s0, reinvest)
+					# вычисление абсолютных доходностей
+					temp.data <- calc.returns(temp.data, pip, s0, abs=TRUE, reinvest)
+					# вычисление SR и запись значений
+					temp.data2 <- calc.returns(temp.data, pip, s0, SR=TRUE, reinvest)
+					temp.data$SR <- temp.data2$SR
+					temp.data$SR.margin <- temp.data2$margin
+					temp.data$SR.equity <- temp.data2$equity
+					remove(temp.data2)
+					# расчет метрик продуктивности
+					PerfTable <- PerformanceTable(temp.data$SR)
+					# расчет временных метрик
+
+					# расчет итогового профита
+					profit <- calc.profit(temp.data, s0, reinvest)
+					# формирования итогового блока данных
 					results <- cbind(fast.sma, slow.sma, accel.start, accel.max, s0, profit, (s0 + profit))
 					if(firstRun){
 			  			firstRun <- FALSE
 			  			colnames(results) <- c("Fast SMA","Slow SMA","Accel Start", "Accel Max", "Start balance", "Profit", "End Balance")	
 		  				final.results <- results 
 		  			} else {
-			  		final.results <- rbind(final.results, final.results)
+			  		final.results <- rbind(final.results, results)
 		  			}
 				}
 			}
@@ -127,12 +148,13 @@ dataset.psar.2sma <- function(data, state, pip, s0, abs, SR, LR, reinvest, ) {
 	}
 	return(results)
 }
+
 	
 # лист переменных
 # get.data <- function (ticker="SiH6", from.date, to.date=Sys.Date(), frame="15min")
 # strategy.psar.2sma <- function (data, slow.sma, fast.sma, accel.start=0.02, accel.max=0.2, state=TRUE)
-# returns.calc <- function(data, pip, s0=0, abs=FALSE, SR=FALSE, LR=FALSE, reinvest=TRUE) 
-# profit.calc <- function(data, s0=0, abs==FALSE, SR==FALSE, LR==FALSE, reinvest==FALSE)
+# calc.returns <- function(data, pip, s0=0, abs=FALSE, SR=FALSE, LR=FALSE, reinvest=TRUE) 
+# calc.profit <- function(data, s0=0, abs==FALSE, SR==FALSE, LR==FALSE, reinvest==FALSE)
 #
 data <- get.data(from.date=Sys.Date()-14)
 temp.data <- data
@@ -140,6 +162,6 @@ head(temp.data, 3)
 #
 data <- strategy.psar.2sma(data=data, slow.sma=20, fast.sma=8)
 #
-data <- returns.calc(data=data, pip=1, abs=TRUE, reinvest=FALSE)
-profit <- profit.calc(data=data, reinvest=FALSE)
+data <- calc.returns(data=data, pip=1, abs=TRUE, reinvest=FALSE)
+profit <- calc.profit(data=data, reinvest=FALSE)
 #
