d08ea8b74c4e0e96c48e963ab03c64ca417cdb16
albanec <eugeni.maksimov@yandex.ru>
2016-04-11 00:13:15 +0300

    +1

--- temp/work.projects/MN/PCAspreadMN.R
+++ temp/work.projects/MN/PCAspreadMN.R
@@ -17,7 +17,7 @@ GetData.TickerList(TickerList, from.date, to.date, period)
 # расширение данных 
 TimeExpand.data(TickerList, FrameList, period, description=FALSE)
 # генерация xts для pca (за один TF) 
-data.matrix <- DataPrepareForPCA (TickerList, description, period, tframe, approx, price)
+data <- DataPrepareForPCA (TickerList, description, period, tframe, approx, price)
 # генерация xts для pca (по группам)
 DataPrepareForGroupPCA <- function (GroupList, FrameList, description, period=c("5min", "15min"),  approx, price) {
 	FrameList <- read.csv(FrameList, header = F, stringsAsFactors = F)
@@ -26,9 +26,9 @@ DataPrepareForGroupPCA <- function (GroupList, FrameList, description, period=c(
 	nperiod <- length(period)
 	for (i in 1:nperiod) {
 		for (t in 1:nframe) {
-			data.matrix <- DataPrepareForPCA (TickerList=GroupList, description=description, period=period[i], tframe=tframe[t], approx=approx, price=price)		
+			data <- DataPrepareForPCA (TickerList=GroupList, description=description, period=period[i], tframe=tframe[t], approx=approx, price=price)		
 			#out.name <- as.character(paste("DataMatrix", GroupList, ".", period[i], ".", tframe[t], sep="") )	 
-			#assign(out.name, data.matrix)
+			#assign(out.name, data)
 			#return(out.name)	
 		}	
 	} 
@@ -36,66 +36,60 @@ DataPrepareForGroupPCA <- function (GroupList, FrameList, description, period=c(
 PCAcompute <- function(GroupList, period, tframe) {
 	# выгрузка данных
 	filename <- paste("MergedData.", GroupList, sep="")
-	data.matrix <- ReadCSVtoXTS (name=filename, period, tframe)
+	data <- ReadCSVtoXTS (name=filename, period, tframe)
 	# вычисление PC			
 	cat ("PCA start:", "\t", GroupList, "\n")
-	pca.data <- princomp(data.matrix)
+	pca.data <- princomp(data)
+	# нагрузки
 	loadings <- pca.data$loadings[]
+	# ???
 	evec <- pca.dates$rotation[] #eigen vectors
 	eval <- pca.dates$sdev^2 #eigen values
 	inv.evec <- solve(evec) #inverse of eigenvector
 	pc.port <- inv.evec %*% t(ret)
+	# ???
 	# Критерий  Кайзера-Гуттмана  рекомендует оставить для дальнейшего анализа только те главные компоненты,
 	# собственные значения которых превышают среднее (собственных значений):
 	#ev <- pca.data$sdev
 	#ev <- which.max(ev>mean(ev))
 	#cat("PCA numbers", ev)
-	# выбираем первые 10 (основное !!! )
-	components <- loadings[,1:10] 
-	# normalize all selected components to have total weight = 1
-	components <- components / rep.row(colSums(abs(components)), ncol(data.matrix))
+	# выбираем не больше первых 10 (основное !!! )
+	n.PC <- ncol(loadings)
+	if (n.PC > 10) {
+		n.PC <- 10
+	} 
+	components <- loadings[,1:n.PC]
+	# нормализуем компоненты - в сумме должны дать 1 (получаем веса)
+	n <- ncol(data)
+	components <- components / rep.row(colSums(abs(components)), n)
+	# note that first component is market, and all components are orthogonal i.e. not correlated to market
+	market <- data %*% rep(1/n,n)
+	temp <- cbind(market, data %*% components)
+    colnames(temp)[1] <- 'Market' 
+    temp <- as.xts(temp, index(data))
+    # корреляция между PC (по Пирсону)
+	cor.table <- round(cor(temp, use='complete.obs',method='pearson'),2)
+ 	# дисперсия компонент
+	vol.table <- round(100*sd(temp,na.rm=T),2)
+	# проверка на стационарность получившихся PC
+	if (DF.test==TRUE) {
+		m <- 1 + (-data %*% components)
+		equity <- apply(m, 2,  cumprod)
+		equity <- as.xts(equity, index(data))
+ 		# test for stationarity ( mean-reversion )
+		df.value <- rep(NA, n.PC)
+		for (i in 1:n.PC) {
+			df.value[i] <- adf.test(as.numeric(equity[, i]))$p.value			
+		}
+		statPC <- which.min(df.value)
+		cat("Best DF-test result...", "\t", df.value[statPC], "\t", "PC:", statPC, "\n")
+	} 
+		# сохраняем нагрузки 
 	loadings.filename <- print("Loadings", Grouplist, period, tframe, "csv", sep=".")
 	write.table(loadings, file=loadings.filename, sep=",")
+	#
 	components.filename <- print("Components", Grouplist, period, tframe, "csv", sep=".")
 	write.table(components, file=components.filename, sep=",")
 	barplot(height=pca.data$sdev[1:10]/pca.data$sdev[1])
 }
-
-VisualizePCA <- 
-# вычисление PC
-p <- princomp(data.matrix)
-# веса
-loadings <- p$loadings[]
-# выбираем первые 10 (основное !!! )
-components <- loadings[] 
-# normalize all selected components to have total weight = 1
-components <- components / rep.row(colSums(abs(components)), ncol(data.matrix))
-
-# note that first component is market, and all components are orthogonal i.e. not correlated to market
-n <- ncol(data.matrix)
-market <- data.matrix %*% rep(1/n,n)
-temp <- cbind(market, data.matrix %*% components)
-colnames(temp)[1] = 'Market'   
-# проверка на корреляцию
-round(cor(temp, use='complete.obs',method='pearson'),2)
-# the variance of each component is decreasing
-round(100*sd(temp,na.rm=T),2)
-#
-#*****************************************************************
-# исследование PC на стационарность (DF-тест)
-#******************************************************************     
-# считаем equity
-m <- 1 + (-data.matrix %*% components)
-equity <- apply(m, 2,  cumprod)
-equity <- as.xts(equity, index(data.matrix))
-# тест на стационарность
-for (i in 1:10) {
-	a <- adf.test(as.numeric(equity[, i]))
-}
-#*****************************************************************
-#Графики
-#*****************************************************************
-
-
-
 
