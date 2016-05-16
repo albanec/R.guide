0eba6324ad0b19a975fda6340c4201e46a79997a
albanec <eugeni.maksimov@yandex.ru>
2016-05-16 19:05:41 +0300

    +1

--- temp/work.projects/cluster/cluster_full.R
+++ temp/work.projects/cluster/cluster_full.R
@@ -1,8 +1,8 @@
-file.path="/home/evgeni/Templates/temp/t4/11.csv"
+file.path="/home/evgeni/Templates/temp/t4/15.csv"
 data <- GEN_AllPreparationLabsFile(file.path=file.path, var.list = c(26, 27, 28), profit=3, draw=9, m=12, q.hi=0.5)
 data$profit <- NULL
 data$draw <- NULL
-clustPar.data <- CLU_CalcClusterParameters(data)
-CLU_ChartSSFrame(ss.df = clustPar.data[1], n.opt = clustPar.data[[2]])
-clustFull.data <- CLU_CalcCluster(data, clustPar.data[[2]])
+clustPar.data <- CLU_CalcKmean.Parameters(data, iter.max = 100, plusplus = TRUE)
+CLU_ChartKmean.SS(ss.df = clustPar.data[1], n.opt = clustPar.data[[2]])
+clustFull.data <- CLU_CalcKmean(data, clustPar.data[[2]], plusplus = TRUE )
 clustFull.data
